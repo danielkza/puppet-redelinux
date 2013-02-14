0d83aa88d41a9105b5608c93f84fa1f6408a1476
root <root@danielqm.linux.ime.usp.br>
2013-02-14 05:03:30 -0200

    Correção de configurações para instalação de múltiplos pacotes

--- modules/redelinux/manifests/kerberos.pp
+++ modules/redelinux/manifests/kerberos.pp
@@ -1,15 +1,16 @@
 class redelinux::kerberos
 {
-	# Kerberos
-	package { 'kerberos':
-		name   => ["krb5-user", "libpam-krb5"],
-		ensure => present
-	}
+    # Kerberos
+    $kerberos = ["krb5-user", "libpam-krb5"]
 
-	file { 'krb5.conf':
-	    ensure  => file,
-		path    => '/etc/krb5.conf',
-		source  => 'puppet:///modules/redelinux/etc/krb5.conf',
-		require => Package['kerberos']
-	}
-}	
+    package { $kerberos:
+        ensure => present,
+    }
+
+    file { 'krb5.conf':
+        ensure  => file,
+        path    => '/etc/krb5.conf',
+        source  => 'puppet:///modules/redelinux/etc/krb5.conf',
+        require => Package[$kerberos],
+    }
+}    
--- modules/redelinux/manifests/ldap.pp
+++ modules/redelinux/manifests/ldap.pp
@@ -1,37 +1,38 @@
 class redelinux::ldap
 {
-	include nsswitch
-	
-	Package['ldap'] -> Class['nsswitch']
-		
-	# LDAP
-	package { 'ldap':
-		name   => ['libnss-ldapd', 'libsasl2-modules-gssapi-mit', 'kstart'],
-		ensure => present
-	}
-	
-	package { 'libpam-ldapd':
-		ensure  => absent,
-		require => Package['ldap']
-	}
-	
-	file { 'ldap.conf':
-	    ensure  => absent,
-		path    => '/etc/ldap/ldap.conf',
-		require => Package['ldap']
-	}
-			
-	file { 'nslcd.conf':
-		ensure  => file,
-		path    => '/etc/nslcd.conf',
-		source  => 'puppet:///modules/redelinux/etc/nslcd.conf',
-		require => Package['ldap']
-	}
+    include nsswitch
+            
+    # LDAP
+    $ldap = ['libnss-ldapd', 'libsasl2-modules-gssapi-mit', 'kstart']
 
-	service { 'nslcd':
-		ensure    => running,
-		enable    => true,
-		require   => Package['ldap'],
-		subscribe => File['nslcd.conf']
-	}
+    package { $ldap:
+        ensure  => present,
+    }
+    
+    Package[$ldap] -> Class['nsswitch'] 
+    
+    package { 'libpam-ldapd':
+        ensure  => absent,
+        require => Package[$ldap],
+    }
+    
+    file { 'ldap.conf':
+        ensure  => absent,
+        path    => '/etc/ldap/ldap.conf',
+        require => Package[$ldap],
+    }
+            
+    file { 'nslcd.conf':
+        ensure  => file,
+        path    => '/etc/nslcd.conf',
+        source  => 'puppet:///modules/redelinux/etc/nslcd.conf',
+        require => Package[$ldap],
+    }
+
+    service { 'nslcd':
+        ensure    => running,
+        enable    => true,
+        require   => Package[$ldap],
+        subscribe => File['nslcd.conf'],
+    }
 }
