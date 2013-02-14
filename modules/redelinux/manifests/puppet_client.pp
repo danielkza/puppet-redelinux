115adcc04ade433338119fa26c375827043c86ec
root <root@danielqm.linux.ime.usp.br>
2013-02-14 05:00:59 -0200

    Correção de opção que impedia início do cliente do puppet no boot

--- modules/redelinux/manifests/puppet_client.pp
+++ modules/redelinux/manifests/puppet_client.pp
@@ -1,12 +1,22 @@
 class redelinux::puppet_client
 {
-	file { 'puppet.conf':
-		path => '/etc/puppet/puppet.conf',
-		source => 'puppet:///modules/redelinux/etc/puppet/puppet.conf'
-	}
-	
-	service { 'puppet':
-		ensure => running,
-		subscribe => File['puppet.conf']
-	}
+    file { 'puppet.conf':
+        ensure => file,
+        path => '/etc/puppet/puppet.conf',
+        source => 'puppet:///modules/redelinux/etc/puppet/puppet.conf',
+    }
+    
+    file { 'puppet_default':
+        ensure => file,
+        path => '/etc/default/puppet',
+        source => 'puppet:///modules/redelinux/etc/default/puppet',
+    }
+    
+    service { 'puppet':
+        ensure => running,
+        subscribe => [
+            File['puppet.conf'],
+            File['puppet_default'],
+        ],
+    }
 }
