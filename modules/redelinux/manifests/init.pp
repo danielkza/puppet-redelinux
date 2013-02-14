53ee64b9ee6595697d36e45fe9a2a562a6b0773b
root <root@danielqm.linux.ime.usp.br>
2013-02-14 05:04:19 -0200

    Novas configurações adicionadas à configuração principal

--- modules/redelinux/manifests/init.pp
+++ modules/redelinux/manifests/init.pp
@@ -1,8 +1,10 @@
 class redelinux
 {
-	include redelinux::network
-	include redelinux::ntp
-	include redelinux::kerberos
-	include redelinux::ldap
-	include redelinux::nfs
+    include redelinux::network
+    include redelinux::ntp
+    include redelinux::kerberos
+    include redelinux::ldap
+    include redelinux::nfs
+    include redelinux::ssh
+    include redelinux::sudo
 }
