ca3cacab7723ce70512bfbd95ddd602ee8855c9b
Daniel Miranda <danielkza2@gmail.com>
2013-03-03 07:34:55 -0300

    Adição da primeira versão dos pacotes de programação

---
+++ modules/redelinux/manifests/programming/ides.pp
@@ -0,0 +1,31 @@
+class redelinux::programming:ides
+{   
+    include redelinux::apt
+
+    Package {
+        ensure => latest,
+    }
+
+    # Eclipse
+    package { 'eclipse': }
+    # Code-blocks
+    if $redelinux::debian_pre_wheezy
+    {
+        apt::force { 'codeblocks': 
+            release  => 'wheezy',
+            require  => Apt::Source['debian_wheezy'],
+            before   => Package['codeblocks'],
+        }
+    }
+    package { 'codeblocks': }
+    # Monodevelop
+    package { 'monodevelop': }
+    # BlueJ
+    package { 'bluej':
+        source => 'http://www.bluej.org/download/files/bluej-309.deb',
+    }
+
+    # Not IDEs, but as good as
+
+
+}
---
+++ modules/redelinux/manifests/programming/languages.pp
@@ -0,0 +1,68 @@
+class redelinux::programming::languages
+{
+    include redelinux::apt
+
+    Package {
+        ensure => latest,
+    }
+
+    # Python
+    package { ['python', 'python3']: }
+    # Ruby
+    package { 'ruby': }
+    # Lua
+    package { 'lua50', 'lua5.1', 'lua5.2': }
+    # Fortran
+    package { 'gfortran': }
+    # C, C++
+    package { ['gcc', 'g++', 'libc-dev']: }
+    package { 'clang': }
+    package { ['make', 'automake', 'autoconf']: }
+    package { ['cmake', 'cmake-curses-gui', 'cmake-qt-gui']: }
+    # Haskell
+    package { 'haskell-platform': }
+    # Erlang
+    package { 'erlang': }
+    # Java
+    if $redelinux::debian_pre_wheezy
+    {
+        package { 'openjdk-6-jdk': }
+    } else {
+        package { 'openjdk-7-jdk': }
+    }
+    package { 'ant': }
+    package { 'maven2': }
+    # Scala
+    package { 'scala': }
+    # PHP
+    package { 'php-cli': }
+    # R
+    package { 'r-recommended': }
+    # Octave
+    package { 'octave': }
+    # Smalltalk
+    package { 'squeak-vm': }
+    file { 'squeak-image':
+        ensure  => directory,
+        path    => '/usr/share/squeak/',
+        source  => 'puppet:///modules/redelinux/usr/share/squeak/',
+        recurse => remote,
+        links   => manage,
+    }
+    # Mono
+    package { 'mono-complete': }
+    # Assembly
+    package { 'nasm', 'gas', 'yasm': }
+    # Lisp
+    package { 'clisp': }
+    # Scheme
+    if $redelinux::debian_pre_wheezy
+    {
+        apt::force { 'racket': 
+            release  => 'squeeze-backports',
+            require  => Apt::Backports,
+            before   => Package['racket'],
+        }
+    }
+    package { 'racket': }
+}
