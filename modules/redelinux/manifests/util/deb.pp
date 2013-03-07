f43819f8c2c5909cdaa3b8d42c3420d6dc393ffb
Daniel Miranda <danielkza2@gmail.com>
2013-03-07 16:47:53 -0300

    deb: tentativa de criação automática de diretórios

--- modules/redelinux/manifests/util/deb.pp
+++ modules/redelinux/manifests/util/deb.pp
@@ -1,27 +1,52 @@
+define redelinux::util::deb::create_file_dir(
+    $path  = $title,
+    $owner = undef,
+    $group = undef,
+    $mode  = undef
+)
+{
+    if !validate_absolute_path($path) {
+        fail("Invalid path ${path}")
+    }
+    
+    $path_parent = regsubst($path, '/[^/]*$', '')
+    if $path_parent != "" {
+        file { $path_parent:
+            ensure => directory,
+            owner  => $owner,
+            group  => $group,
+            mode   => $mode,
+        }
+        
+        create_file_dir { $path_parent:
+            owner => $owner,
+            group => $group,
+            mode  => $mode,
+        }      
+    }  
+}
+
 define redelinux::util::deb(
     $source = "puppet:///modules/${module_name}/packages/${name}.deb",
     $path   = undef,
     $ensure = present
 ) {
     if $path == undef {
-        @file { "deb::${name}::default_install_path":
-            ensure => directory,
-            path   => "/var/${module_name}-packages/",
-            before => "deb::${name}",
-        }
-        
         $path_real = "/var/${module_name}-packages/${name}.deb"
     } else {
         $path_real = $path
     }
+    
+    redelinux::util::deb::create_file_dir { $path_real: }
 
-    file { "deb::${name}":
+    file { "deb::${title}":
         ensure => file,
         source => $source,
         path   => $path_real,
     }
 
-    package { $name:
+    package { $title:
+        name     => $name,
         ensure   => $ensure,
         provider => dpkg,
         source   => $path_real,
