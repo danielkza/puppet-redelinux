ad6101162dd2041d54c5245c7b0dfa4a17052fae
Daniel Miranda <danielkza2@gmail.com>
2013-03-11 16:46:51 -0300

    kerberos: primeira tentativa de k5login

--- modules/redelinux/manifests/kerberos.pp
+++ modules/redelinux/manifests/kerberos.pp
@@ -13,4 +13,18 @@ class redelinux::kerberos
         path    => '/etc/krb5.conf',
         require => Package[$kerberos],
     }
+    
+    $group = $redelinux::params::kerberos_admin_group
+    $realm = $redelinux::params::kerberos_realm
+    
+    $users_line = generate('getent', 'group', $group)
+    $users_list = regsubst($users_line, '^.+:', '')
+    $principals = split(regsubst(',', "/admin@${realm}",'), ',')
+    notice("principals: ${principals}")
+    
+    #k5login { 'redelinux':
+    #    ensure     => present,
+    #    path       => '/root/.k5login',
+    #    principals => $principals
+    #}    
 }    
--- modules/redelinux/manifests/params.pp
+++ modules/redelinux/manifests/params.pp
@@ -3,4 +3,7 @@ class redelinux::params
     $debian_pre_wheezy = ($::lsbdistid == 'Debian'
                           and versioncmp($::lsbmajdistrelease, '7') < 0)
     $debian_mirror = 'http://sft.if.usp.br/debian/'
+    
+    $kerberos_admin_group = 'olimpo'
+    $kerberos_realm = 'LINUX.IME.USP.BR'
 }
