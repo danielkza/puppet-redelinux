a7972e469a6b95d65f2a8250d5a7415195245f0c
Daniel Miranda <danielkza2@gmail.com>
2013-03-11 02:13:54 -0300

    puppet_client: mudança de daemon para cron

--- modules/redelinux/manifests/params.pp
+++ modules/redelinux/manifests/params.pp
@@ -3,4 +3,7 @@ class redelinux::params
     $debian_pre_wheezy = ($::lsbdistid == 'Debian'
                           and versioncmp($::lsbmajdistrelease, '7') < 0)
     $debian_mirror = 'http://sft.if.usp.br/debian/'
+
+    $puppet_client_command = '/usr/bin/puppet agent --onetime --no-daemonize'
+    $puppet_client_run_interval = 30
 }
