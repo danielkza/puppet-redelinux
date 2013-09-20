0fd26eb96535b645f41bc75ec476ea410d0d7649
Daniel Miranda <danielkza2@gmail.com>
2013-09-20 11:09:59 -0300

    Update debian repo options

--- modules/redelinux/manifests/params.pp
+++ modules/redelinux/manifests/params.pp
@@ -3,8 +3,12 @@ class redelinux::params(
     
     $debian_pre_wheezy = ($::lsbdistid == 'Debian'
                           and versioncmp($::lsbmajdistrelease, '7') < 0),
-    $debian_mirror     = 'http://sft.if.usp.br/debian/',
     
+    $debian_mirror        = 'http://sft.if.usp.br/debian/',
+    $debian_use_backports = false
+    $debian_use_testing   = false
+
+
     $kerberos_admin_group = 'olimpo',
     $kerberos_realm       = 'LINUX.IME.USP.BR',
 
