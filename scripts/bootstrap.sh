ecb2047f80ea9b47ce1a42a6135819733690998f
Daniel Miranda <danielkza2@gmail.com>
2013-10-31 15:38:03 -0200

    Adiciona script de bootstrap p/ instalação

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+codename=$(lsb_release -c -s)
+package="puppetlabs-release-$codename.deb"
+
+wget "http://apt.puppetlabs.com/$package"
+dpkg -i $package
+apt-get update
+apt-get install -y puppet
