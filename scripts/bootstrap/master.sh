62cb8454cf3734c1b4d175ffe17ba1261d90ae05
Bison Root <danielkza2@gmail.com>
2013-12-06 15:55:01 -0200

    bootstrap: atualiza master.sh

--- scripts/bootstrap/master.sh
+++ scripts/bootstrap/master.sh
@@ -5,12 +5,7 @@ set -ex
 base_dir=$(readlink -f $(dirname "$0"))
 cd "$base_dir"
 
-puppet_dir=$(readlink -f "$base_dir/../../")
-module_dir="$puppet_dir/modules" 
-if ! [ -d "$module_dir" ]; then
-	echo "Erro: diretório modules não encontrado em '$puppet_dir'"
-	exit 1
-fi
-
 ./bootstrap.sh
-puppet apply --noop --modulepath="$module_dir" master.pp
+
+puppet module install theforeman-puppet
+puppet apply --noop master.pp
