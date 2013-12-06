7b3937718e9baff022d6c3eb81adae13a0da922e
Bison Root <danielkza2@gmail.com>
2013-12-06 15:52:56 -0200

    bootstrap: atualiza master.pp

--- scripts/bootstrap/master.pp
+++ scripts/bootstrap/master.pp
@@ -1,16 +1,12 @@
 class { '::puppet':
     server           => true,
     server_passenger => true,
+    
     server_ca        => false,
+    ca_server        => 'foreman.linux.ime.usp.br',
     server_storeconfigs_backend => 'puppetdb',
     
     server_enc_api     => 'v2',
     server_report_api  => 'v2',
-    server_foreman_url => 'https://elise.linux.ime.usp.br',
-}
-
-class { 'foreman::puppetmaster':
-	foreman_url => "https://elise.linux.ime.usp.br",
-	reports => true,
-	enc     => true,
+    server_foreman_url => 'https://foreman.linux.ime.usp.br',
 }
