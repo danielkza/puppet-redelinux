9b58dcddcf6d0eb41d6ea1d3cafda12a9cdc928f
Daniel Miranda <danielkza2@gmail.com>
2014-10-06 14:20:58 -0300

    ldap wip

--- site-modules/redelinux/manifests/domain/ldap.pp
+++ site-modules/redelinux/manifests/domain/ldap.pp
@@ -3,38 +3,35 @@ class redelinux::ldap
   Redelinux::Util::Cfg_file {
     source_prefix => "ldap"
   }
-
-  # LDAP
-  $ldap = ['libnss-ldapd', 'kstart']
-
-  package { $ldap:
-    ensure => present,
-  }
-
   # We only want LDAP authentication, authorization is Kerberos' job
-  package { 'libpam-ldapd':
-    ensure  => absent,
-    require => Package[$ldap],
+  package {
+    ['libnss-ldapd', 'kstart']:
+      ensure => installed,
+      tag    => 'ldap-pkg';
+    'libpam-ldapd':
+      ensure => absent,
+      tag    => 'ldap-pkg';
   }
 
   service { 'nslcd':
     ensure  => running,
-    enable  => true,
-    require => Package[$ldap],
+    enable  => true
   }
 
-  # LDAP config files
   file { '/etc/ldap/ldap.conf':
     ensure  => absent,
-    require => Package[$ldap],
+    tag     => 'ldap-cfg'
   }
 
   util::cfg_file { '/etc/nslcd.conf':
-    require => Package[$ldap],
-    notify  => Service['nslcd'],
+    tag => 'ldap-cfg'
   }
 
   nsswitch::database { ['passwd', 'group', 'shadow']:
     services => 'files ldap',
+    tag      => 'ldap-cfg'
   }
+
+  Package <| tag == 'redelinux::ldap' |> ->
+    Service <| tag == 'redelinux::ldap' |>
 }
