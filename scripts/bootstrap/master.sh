538dedd087888594d70d00972892def0f1a05111
Daniel Miranda <danielkza2@gmail.com>
2014-10-06 18:18:41 -0300

    bootstrap: master: adiciona puppetdb

--- scripts/bootstrap/master.sh
+++ scripts/bootstrap/master.sh
@@ -6,4 +6,5 @@ base_dir=$(readlink -f "$(dirname "${BASH_SOURCE[0]}")")
 "${base_dir}/bootstrap.sh"
 
 puppet module install theforeman-puppet
+puppet module install puppetlabs-puppetdb
 puppet apply --noop master.pp
