9b5add10c521e9a4d65030fdf624fab7a7753b98
Daniel Miranda <danielkza2@gmail.com>
2014-10-06 18:18:07 -0300

    mozilla: checa melhor debian

--- site-modules/redelinux/manifests/packages/desktop/mozilla.pp
+++ site-modules/redelinux/manifests/packages/desktop/mozilla.pp
@@ -4,25 +4,28 @@ class redelinux::packages::desktop::mozilla
     ensure => installed
   }
 
-  if $::operatingsystem == 'Debian' && $::lsbmajdistrelease <= 7 { 
-    apt::source { 'mozilla':
-      location          => 'http://mozilla.debian.net',
-      repos             => 'iceweasel-release',
-      release           => '${::lsbdistcodename}-backports',
-      required_packages => 'pkg-mozilla-archive-keyring',
-      include_src       => true
-    }
+  if $::operatingsystem == 'Debian' {
+    if is_integer($::lsbmajdistrelease) && $::lsbmajdistrelease <= 7 { 
+      apt::source { 'mozilla':
+        location          => 'http://mozilla.debian.net',
+        repos             => 'iceweasel-release',
+        release           => '${::lsbdistcodename}-backports',
+        required_packages => 'pkg-mozilla-archive-keyring',
+        include_src       => true,
+        before            => Package['iceweasel', 'icedove']
+      }
 
-    apt::pin { 'debian-mozilla':
-      priority  => 501,
-      release   => '${::lsbdistcodename}-backports',
-      component => 'iceweasel-release',
-      order     => 40,
-      before    => Apt::Source['mozilla']
+      apt::pin { 'debian-mozilla':
+        priority  => 501,
+        release   => '${::lsbdistcodename}-backports',
+        component => 'iceweasel-release',
+        order     => 40,
+        before    => Apt::Source['mozilla']
+      }
     }
-
-    Apt::Source['mozilla'] -> Package['iceweasel', 'icedove']
+    
+    package { ['iceweasel', 'icedove']: }
+  } else {
+    package { 'mozilla-firefox': }
   }
-  
-  package { ['iceweasel', 'icedove']: }
 }
